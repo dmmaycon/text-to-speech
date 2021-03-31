5e1c932bac0c72a311f73530882df65c362240be
dmmaycon <mayconmoraes.dm@gmail.com>
2021-03-31 17:36:48 -0300

    initial commit

---
+++ app/js/app.js
@@ -0,0 +1,55 @@
+let   indexVoicesByLanguage = 0;
+let   languages         = window.navigator.languages;
+let   languageDefault   = window.navigator.language;
+const voices            = speechSynthesis.getVoices();
+let   voiceDefault      = voices.filter(voice => voice.lang.includes(languageDefault))[indexVoicesByLanguage];
+let   voicesByLanguage  = null;
+const selectLanguage    = document.getElementById('selectLanguage');
+const selectVoice       = document.getElementById('selectVoice');
+
+
+function initLanguages() {
+    languages = languages.concat(['pt-BR']);
+    languages.forEach(lang => {
+        option = new Option;
+        option.value = lang;
+        option.text  = lang;
+        selectLanguage.add(option);
+    });
+}
+
+function changeLanguage() {
+    languageDefault = languages[selectLanguage.selectedIndex];
+    selectVoice.options.length = 0;
+    initVoices();
+}
+
+function initVoices() {
+    initLanguages();
+    voicesByLanguage      = voices.filter(voice => voice.lang.includes(languageDefault))
+    
+    let index = 0;
+    voicesByLanguage.forEach(voice => {
+        index++;
+        option = new Option;
+        option.value = index;
+        option.text  = voice.name;
+        selectVoice.add(option);
+    });
+
+}
+
+function textToSpeech() {
+    const text = document.getElementById('textToSpeech').value;
+    const utterance  = new SpeechSynthesisUtterance(text);
+    utterance.voice  = voicesByLanguage[selectVoice.selectedIndex];
+    speechSynthesis.speak(utterance);
+    console.log("Voz selecionada: " + selectVoice.selectedIndex);
+    console.log("Idioma selecionado: " + languageDefault);
+}
+
+
+(function () {
+    initVoices();
+})();
+
